26e9a86c65c0c732d0c08cadcfd25e7c3488613d
Maks <nefatovmaksim@gmail.com>
2025-01-09 22:30:48 +0300

    Update 7.7

--- part_7.py
+++ part_7.py
@@ -236,3 +236,16 @@
 #         continue
 #     else:
 #         print(i)
+
+
+
+# #7.7
+# s = 0
+# for _ in range(7):
+#     n = int(input())
+#     if n % 2 == 0:
+#         s = s + n
+# if s != 0:
+#     print(s)
+# else:
+#     print(0)
